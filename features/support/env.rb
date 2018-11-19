3eff82d7bedea6811d2eba00d0476bfc53d4ae14
Gerry Sousa <gerry.nett@gmail.com>
2018-11-19 21:20:06 -0200

    Mudando selenium para ser do chrome ao invés do mozila

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ require 'capybara/cucumber'
 require 'selenium-webdriver'
 
 Capybara.configure do |config|
-  config.default_driver = :selenium
+  config.default_driver = :selenium_chrome
   config.default_max_wait_time = 5
   config.app_host = 'http://mark7.herokuapp.com'
 end
