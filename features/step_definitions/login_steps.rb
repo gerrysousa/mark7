432ad75155d71d35a922aa84e1a98f13820541cc
Gerry Sousa <gerry.nett@gmail.com>
2018-11-19 21:16:18 -0200

    Iniciando testes do login

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,32 @@
+Dado("que eu acessei o formulário de login") do
+  @home.acessar
+end
+  
+  Quando("faço login com {string} e {string}") do |string, string2|
+    puts string
+    puts string2
+    # Quando('faço login com:') do |table|
+      # @usuario = table.rows_hash
+      @login.faz_login(string, string2)
+    # end
+  end
+  
+  Então("sou autenticado com sucesso") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("sou redirecionado para o painel de tarefas com a mensagem {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("devo ver a mensagem de alerta {string}") do |string|
+    #Então("devo ver a seguinte mensagem de {string}") do |mensagem_alerta|
+      expect(@login.msg_alerta).to eql mensagem_alerta
+    #end
+  end
+
+
+ 
+  
+  
+  
--- features/support/pages/login_page.rb
+++ features/support/pages/login_page.rb
@@ -1,5 +1,16 @@
-
-
 class LoginPage
-
-end
+    include Capybara::DSL
+  
+    def faz_login(email, senha)
+      find('#login_email').set email
+      find('#login_password').set senha
+      click_button 'Login'
+    end
+  
+    def msg_alerta
+      within('form[id=login_form]') do
+        return find('.alert').text
+      end
+    end
+  end
+  
